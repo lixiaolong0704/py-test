9330453b48f1ccea1fe0eb86568b0b6ae01d14e9
leibo.sun@ptmind.com <leibo.sun@ptmind.com>
2018-08-08 21:07:40 +0800

    test bjh

---
+++ tutorial/spiders/bjh_spider.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+def printhxs(hxs):
+    for i in hxs:
+        print i.encode('utf-8')
+
+class QuotesSpider(scrapy.Spider):
+    name = "bjh"
+
+    def start_requests(self):
+        urls = [
+            'http://bxjg.circ.gov.cn/web/site0/tab5203/info4112314.htm' 
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        self.log('----I love you------------------------------------------------')
+        trs = response.xpath('//table[@class="MsoTableGrid"]/tbody/tr')
+
+        for index,tr in enumerate(trs):
+            text = tr.xpath('//td/p/span/text()').extract()
+            # text= tr.xpath('//td/p/span/text()').extract()
+            print '******************'
+            for td in text:
+                 printhxs(td)
+            # printhxs(str(text))
+ 
+        
+        filename = 'cicr-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('保存 Saved file %s' % filename)
